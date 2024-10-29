16b7981bf188bb743e873679fe1a1ed1a58f7815
OmarYessid <omaryessid924@gmail.com>
2024-10-29 11:20:29 -0500

    Se crea el objeto vehiculo

---
+++ src/metodos/vehiculo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos;
+
+/**
+ *
+ * @author Omar
+ */
+public class vehiculo {
+    int id;
+    String nombre, marca;
+    float precio;
+
+    public vehiculo() {
+    }
+
+    public vehiculo(int id, String nombre, String marca, float precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
+    
+}
+
